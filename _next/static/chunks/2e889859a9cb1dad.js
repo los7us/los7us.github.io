6fa122ee32bfa5515878d132fdd3c06bb915407e
Aloysius Akagre <83490265+los7us@users.noreply.github.com>
2026-03-10 05:07:41 +0000

    feat: update portfolio to include details and minor ui adjustments

---
+++ _next/static/chunks/2e889859a9cb1dad.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,16868,i=>{"use strict";var t=i.i(43476),e=i.i(46932);function o({children:i}){return(0,t.jsx)(e.motion.div,{initial:{opacity:0,y:8},animate:{opacity:1,y:0},transition:{duration:.3,ease:"easeOut"},children:i})}i.s(["default",()=>o])}]);
